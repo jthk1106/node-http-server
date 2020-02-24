97b1d804f299a1aa851b53ee5b23f53c58b4ec76
Jeremy Kim <thk116@gmail.com>
2020-02-24 11:23:09 -0800

    completed hello world

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // write your code here
+const http = require('http');
+
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World')
+});
+
+server.listen(PORT, 'localhost', null, () => {
+  console.log(`Server is listening on localhost:${PORT}`)
+});
